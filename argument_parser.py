9dbdafa992ba393efb2f64299e92d82f8ad061e2
Iain Maryanow <iainmaryanow@gmail.com>
2020-03-12 11:59:35 +0100

    Update arguments to allow single pdb file or directory with list of interactions

--- argument_parser.py
+++ argument_parser.py
@@ -3,16 +3,41 @@ import os
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='SBI Complex Builder')
-
-    parser.add_argument('--input_directory', action='store', default=None, dest='input_directory', help='Directory location of PDB files')
+    parser.add_argument('--input_directory', action='store', default=None, dest='input_directory', help='Directory of chain interactions')
+    parser.add_argument('--input_pdb', action='store', default=None, dest='input_pdb', help='Location of PDB to test')
     parser.add_argument('--output_directory', action='store', default=None, dest='output_directory', help='Directory location to save output files')
     args = parser.parse_args()
 
-    pdb_input_files = get_pdb_files_in_directory(args.input_directory)
-    if not len(pdb_input_files):
-        raise Exception('%s does not contain any .pdb files' % args.input_directory)
+    if args.input_pdb is None and args.input_directory is None:
+        raise Exception('One of --input_pdb and --input_directory must be given')
 
-    return pdb_input_files, args.output_directory
+    if args.input_pdb is not None and args.input_directory is not None:
+        raise Exception('Cannot run with both a PDB file and a directory of chain interactions')
+
+    if args.output_directory is None:
+        raise Exception('Output directory is required')
+
+    inputs = {
+        'output_directory': args.output_directory
+    }
+
+    if args.input_pdb is not None:
+        if not os.path.isfile(args.input_pdb):
+            raise Exception('%s does not exist' % args.input_pdb)
+
+        inputs['pdb'] = args.input_pdb
+
+    if args.input_directory is not None:
+        chain_interaction_files = get_pdb_files_in_directory(args.input_directory)
+        if not len(chain_interaction_files):
+            raise Exception('%s does not contain any .pdb files' % args.input_directory)
+
+        inputs['chain_interactions'] = chain_interaction_files
+
+    if not os.path.isdir(args.output_directory):
+        os.mkdir(args.output_directory)
+
+    return inputs
 
 def get_pdb_files_in_directory(directory):
     if not os.path.isdir(directory):
