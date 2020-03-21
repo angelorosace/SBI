23e581ebba49cc2c0de05d14f1f38bee0785a677
Iain Maryanow <iainmaryanow@gmail.com>
2020-03-21 12:16:14 +0100

    Update recursion and write check for stoichiometry

--- structure.py
+++ structure.py
@@ -76,56 +76,63 @@ def has_clashes_with_structure(structure, atoms, clash_distance=2, minimum_atoms
 
 
 
-# TODO
-#   Figure out structure of stoichiometry
-#   Loop through every current chain in structure to see if adding this chain is valid
+# The structure and chain are compatible if adding this chain id onto the structure
+# is still within the boundaries set in the stoichiometry. If the chain id is not
+# in the stoichiometry, it is considered as not valid to be in the structure.
 def is_compatible_with_stoichiometry(structure, chain, stoichiometry):
-    return True
+    chain_id = chain.id
+    chains = list(structure.get_chains())
+    current_chain_count = len([chain for chain in chains if chain.id == chain_id])
+    return chain_id in stoichiometry and \
+           current_chain_count + 1 <= stoichiometry[chain_id]
 
-def recursively_add_chains_to_structure(structure, chains, stoichiometry=None):
-    has_too_many_chains = len(list(structure.get_chains())) >= MAX_CHAINS_IN_STRUCTURE
-    should_stop_recursion = has_too_many_chains if stoichiometry is None else \
-                            not is_compatible_with_stoichiometry(structure, chain, stoichiometry)
 
-    if should_stop_recursion:
-        return structure, 0
+# Recursively check all possible structures given the chains that are compatible with
+# the stoichiometry and do not have clashes after superimposing.
+def recursively_add_chains_to_structure(structure, chains, stoichiometry=None, rmsd=0):
+    if stoichiometry is None and len(list(structure.get_chains())) >= MAX_CHAINS_IN_STRUCTURE:
+        return structure, rmsd
 
     # Keep track of the structure that was determined as the best by lowest RMSD
     best_structure = None
     best_rmsd = float('inf')
 
-    for chain in chains:
-        # Copy the two objects to not modify the originals
-        structure_atoms = list(structure.copy().get_atoms())
-        chain_atoms = list(chain.copy().get_atoms())
+    for chain in [chain.copy() for chain in chains]:
+        # Do not attempt to use this chain if it is not compatible with given stoichiometry
+        if stoichiometry and not is_compatible_with_stoichiometry(structure, chain, stoichiometry):
+            continue
+
+        structure_atoms = list(structure.get_atoms())
+        chain_atoms = list(chain.get_atoms())
 
         # Ensure that the lists of atoms are of the same length
-
-# We may want to change this, such as below with a sliding window or checking which atoms on different chains
+        #
+        #       TODO - Figure out a better way to do this
+        #
         imposition_length = min(len(structure_atoms), len(chain_atoms))
         fixed_atoms = structure_atoms[:imposition_length]
         moving_atoms = chain_atoms[:imposition_length]
 
-        # Superimpose and move the atoms in the moving_atoms list to the best rotation/translation
+        # Superimpose and transform the atoms in the moving_atoms list to the best rotation/translation
         superimposer = Superimposer()
         superimposer.set_atoms(fixed_atoms, moving_atoms)
         superimposer.apply(moving_atoms)
 
+        # Continue building the structure with the added chain if it does not clash
         if not has_clashes_with_structure(structure, moving_atoms):
             structure_copy = structure.copy()
             structure_copy.add(chain)
-            resulting_structure, rmsd = recursively_add_chains_to_structure(structure_copy, chains)
+            resulting_structure, resulting_rmsd = recursively_add_chains_to_structure(structure_copy, chains, stoichiometry, superimposer.rms)
 
             # Update the best structure found so far if the best substructure
             # and current superimposed structure have a lower RMSD
-
-# ??? Do we need to sum the total rmsd or would superimposing take that into account?
-            total_rmsd = rmsd + superimposer.rms
-            if total_rmsd < best_rmsd:
+            if resulting_rmsd < best_rmsd:
                 best_structure = resulting_structure
-                best_rmsd = total_rmsd
+                best_rmsd = resulting_rmsd
 
-    return best_structure, best_rmsd
+    # Return the best structure and RMSD if one is found. If all chains were
+    # incompatible or had clashes, return the last seen structure
+    return (best_structure, best_rmsd) if best_structure else (structure, rmsd)
 
 
 
