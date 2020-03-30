47fdefa63430cec3c950921e9ba3ce81c1288d51
Iain Maryanow <iainmaryanow@gmail.com>
2020-03-30 19:00:59 +0200

    Clean up code

--- macro_molecular_complex_builder.py
+++ macro_molecular_complex_builder.py
@@ -1,19 +1,4 @@
 from argument_parser import parse_arguments
 
-from Bio.PDB.PDBIO import PDBIO
-from Bio.PDB import PDBParser
-
-from structure_builder import testing, recursively_add_chains_to_structure, recurse
-
 if __name__ == '__main__':
-    # inputs = parse_arguments()
-
-    m, i = testing()
-
-    model, rmsd = recurse(m, i)
-
-    print(list(model.get_chains()))
-
-    io=PDBIO()
-    io.set_structure(model)
-    io.save('out.pdb')
+    inputs = parse_arguments()
