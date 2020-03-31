725351bb175dde599f5a08200a1a0e6a5b063690
Iain Maryanow <iainmaryanow@gmail.com>
2020-03-31 14:57:07 +0200

    Add prnt statements

--- macro_molecular_complex_builder.py
+++ macro_molecular_complex_builder.py
@@ -1,13 +1,7 @@
 from argument_parser import parse_arguments
-from printer import set_is_verbose, prnt
-
-from structure_builder import testing
+from printer import set_is_verbose
 
 if __name__ == '__main__':
     inputs = parse_arguments()
     if inputs['verbose']:
       set_is_verbose()
-
-    testing()
-
-    prnt('done for', 'now')
